a400760649f47c9e13699f5fa414d33b88f44df2
elenacary <eecary@email.sc.edu>
2026-09-24 13:34:23 -0400

    trying

--- lab5/script.js
+++ lab5/script.js
@@ -1,8 +1,8 @@
 let playerMoney = 0;
-const addedAmount = 30;
+const addedAmount = 100;
 
 function Deposit() {
-    playerHealth = playerMoney + addedAmount;
+    playerMoney = playerMoney + addedAmount;
 
     const cashText = document.getElementById("cash-display");
     const statusText = document.getElementById("status-message");
@@ -25,6 +25,37 @@ function Deposit() {
         document.body.style.backgroundColor = "#5a1a1a";
 
         document.querySelection("button").disabled = true;
-        document.querySelection("button").innerText = "Dead";
+        document.querySelection("button").innerText = "Broke";
+    }
+}
+
+let playerMoney = 0;
+const removedAmount = 20;
+
+function Withdraw() {
+    playerMoney = playerMoney - removedAmount;
+
+    const cashText = document.getElementById("cash-display");
+    const statusText = document.getElementById("status-message");
+
+    healthText.innerText = playerMoney;
+
+    if(playerMoney < 0)
+    {
+        healthText.innerText = playerMoney;
+        statusText.innerText = "You have overdrawn!";
+
+    }
+    else
+    {
+        healthText.innerText = 0;
+        statusText.innerText = "Great work saving!";
+        statusText.style.color = "#1df98b";
+        statusText.style.fontWeight = "bold";
+
+        document.body.style.backgroundColor = "#5a1a1a";
+
+        document.querySelection("button").disabled = true;
+        document.querySelection("button").innerText = "Savings";
     }
 }
