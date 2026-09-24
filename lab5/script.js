4bbbec64cc3be52d08ffbfa48ce42166eef0d6b7
elenacary <eecary@email.sc.edu>
2026-09-24 13:28:12 -0400

    money

---
+++ lab5/script.js
@@ -0,0 +1,30 @@
+let playerMoney = 0;
+const addedAmount = 30;
+
+function Deposit() {
+    playerHealth = playerMoney + addedAmount;
+
+    const cashText = document.getElementById("cash-display");
+    const statusText = document.getElementById("status-message");
+
+    healthText.innerText = playerMoney;
+
+    if(playerMoney > 0)
+    {
+        healthText.innerText = playerMoney;
+        statusText.innerText = "You have deposited money!";
+
+    }
+    else
+    {
+        healthText.innerText = 0;
+        statusText.innerText = "Don't spend any money!";
+        statusText.style.color = "#1df98b";
+        statusText.style.fontWeight = "bold";
+
+        document.body.style.backgroundColor = "#5a1a1a";
+
+        document.querySelection("button").disabled = true;
+        document.querySelection("button").innerText = "Dead";
+    }
+}
