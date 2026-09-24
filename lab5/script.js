3b43a09480509b261add6cc5368d4a48a481fec9
elenacary <eecary@email.sc.edu>
2026-09-24 13:56:42 -0400

    withdrawn

--- lab5/script.js
+++ lab5/script.js
@@ -12,7 +12,7 @@ function takeWithdraw() {
     if(playerHealth > 0)
     {
         healthText.innerText = playerHealth;
-        statusText.innerText = "You've withdraw money!";
+        statusText.innerText = "You've withdrawm money!";
 
     }
     else
